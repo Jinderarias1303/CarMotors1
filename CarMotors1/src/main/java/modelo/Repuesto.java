d5a5736192f432e891e5a45cde04a0781ca842bc
Jinderarias1303 <sisenornosenor9@gmail.com>
2025-05-04 17:12:54 -0500

    Registro Proveedores Completo

---
+++ CarMotors1/src/main/java/modelo/Repuesto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author jinde
+ */
+public class Repuesto {
+      private int id_repuesto;
+      private String nombre_repuesto;
+      private String tipo_repuesto;
+      
+}
